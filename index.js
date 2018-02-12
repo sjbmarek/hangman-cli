7030a6621b50bf54acb7b2a7f35c81157d40b632
Susan Marek <smarek@chaskamn.com>
2018-02-11 22:01:17 -0600

    added inquirer npm

--- index.js
+++ index.js
@@ -7,46 +7,49 @@ var selectedWord = targetWords[Math.floor(Math.random() * targetWords.length)];
 console.log (selectedWord);
 var unknownWord=[];
 
-var testletter = "a";
+var testletter = "";
 
 
-var output = new Word(testletter,selectedWord);
+// var output = new Word(testletter,selectedWord);
 
 
 inquirer.prompt([
 
-	{
+{
 	type: "list",
 	message: "Play Hangman?",
 	choices: ["yes", "no"],
 	name: "play"
 }
+])
 .then(function(inquirerResponse) {
-    
+	
 
-    if (inquirerResponse.play === "no") {
-    	return
-    }else
-    inquirer.prompt([
+	if (inquirerResponse.play === "no") {
+		return
+	}else
+	inquirer.prompt([
 
-		{
-			type: "input",
-			message: "Guess a Letter: ",
-			name: "guess"
-		}
-		])
-    
-    	.then(function(inquirerResponse) {
-    	
+	{
+		type: "input",
+		message: "Guess a Letter: ",
+		name: "guess"
+	}
+	])
+	
+	.then(function(inquirerResponse) {
+		testletter = inquirerResponse.guess;
+		console.log("You Guessed: " + testletter);
+		var output = new Word(testletter,selectedWord);
 
 
 
 
-    	
-    	});
 
-    	// displayTwitter();
-    }
+	});
+
+	
+})
 
 
 
